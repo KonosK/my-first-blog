1d92948f819b3635c4fd1c6513425bbfab5911c0
Konstantinos <konstantinoskelepeshis@outlook.com>
2025-06-12 14:44:28 +0300

    added cbv

--- blog/views.py
+++ blog/views.py
@@ -3,29 +3,30 @@ from .models import Post
 from django.utils import timezone
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts' : posts})
+from django.views import View
+class PostList(View):
+    def get(self, request):
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+        return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post' : post})
 
-@login_required
-def post_new(request):
-    if request.method == "POST":
+class PostNew(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form' : form})
+    def post(self, request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.save()
             return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form' : form})
 
 @login_required
-def post_edit(request, pk):
+def PostEdit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance = post)
