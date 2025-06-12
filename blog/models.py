d521bb6050aba8ffed2341765a9ab4472171d254
Konstantinos <konstantinoskelepeshis@outlook.com>
2025-06-12 12:35:23 +0300

    title color added

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from colorfield.fields import ColorField
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length = 200)
+    title_color = ColorField(default='#000000')
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null = True)
